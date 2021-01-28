6bfff1311f830d2fb4788fd541040528e5e30aa1
Igna-M <ign.manzo@gmail.com>
2021-01-28 14:42:23 -0300

    Avanzando con el ejercicio, con la pagina de detalles

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -1,49 +1,57 @@
+let menu = [
+    {
+        id: 1,
+        nombre : 'Carpaccio fresco',
+        descripcion : 'Entrada Carpaccio de salmón con cítricos',
+        precio : 65.50,
+        imagen : '/images/Carpaccio-de-salmon.jpg',
+    }, 
+    {
+        id: 2,
+        nombre : 'Risotto de berenjena',
+        descripcion : 'Risotto de berenjena y queso de cabra',
+        precio : 47.00,
+        imagen : '/images/Risotto-berenjena-queso-cabra.jpg',
+    }, 
+    {
+        id: 3,
+        nombre : 'Mousse de arroz',
+        descripcion : 'Mousse de arroz con leche y aroma de azahar',
+        precio : 27.50,
+        imagen : '/images/Mousse-de-arroz-con-leche.jpg',
+    }, 
+    {
+        id: 4,
+        nombre : 'Espárragos blancos',
+        descripcion : 'Espárragos blancos con vinagreta de verduras y jamón ibérico',
+        precio : 37.50,
+        imagen : '/images/esparragos.png',
+    }, 
+    {
+        id: 5,
+        nombre : 'Empanadas',
+        descripcion : undefined,
+        precio : 2.50,
+        imagen : undefined,
+    }, 
+];
+
+
 const controller = {
     index: (req, res) => {
-
-        let menu = [
-                {
-                    nombre : 'Carpaccio fresco',
-                    descripcion : 'Entrada Carpaccio de salmón con cítricos',
-                    precio : 65.50,
-                }, 
-                {
-                    nombre : 'Risotto de berenjena',
-                    descripcion : 'Risotto de berenjena y queso de cabra',
-                    precio : 47.00,
-                }, 
-                {
-                    nombre : 'Mousse de arroz',
-                    descripcion : 'Mousse de arroz con leche y aroma de azahar',
-                    precio : 27.50,
-                }, 
-                {
-                    nombre : 'Espárragos blancos',
-                    descripcion : 'Espárragos blancos con vinagreta de verduras y jamón ibérico',
-                    precio : 37.50,
-                }, 
-            ];
 
         return res.render('index', {'menu': menu});
 
     },
-    
-    // detalles: (req, res) => {
-
-    //     return res.send('Pagina de Detalles');
-    // },
 
     detalles: (req, res) => {
-        if (req.params.id == undefined) {
-            return res.send('Pagina de Detalles');
-        } else {
-            return res.send('Pagina de Detalles ' + req.params.id);
-        }        
+        nID = req.params.id
+        plato = menu.find(platillo => platillo.id == nID)
+
+        return res.render('detalleMenu', {'plato': plato});
+
     },
-
     
-      
-
 }
 
 module.exports = controller;
--- routes/index.js
+++ routes/index.js
@@ -4,17 +4,9 @@ var router = express.Router();
 const mainController = require('../controllers/mainController');
 
 
+
 router.get('/', mainController.index)
 
-router.get('/detalles/:id?', mainController.detalles)
-
-
-// router.get('/detalle/:id', mainController.detalle)
-
-// router.get('/login', mainController.login)
-
-// router.get('/operador', mainController.operador)
-
-
+router.get('/detalles/:id', mainController.detalles)
 
 module.exports = router;
